5c98fe641ef14467dbe017d93be6fd1af4fe8daa
Yamikani Chinamale <ychinamale@gmail.com>
2023-10-31 21:17:19 +0200

    Hook form to store

--- src/widgets/name-form/name-form.component.js
+++ src/widgets/name-form/name-form.component.js
@@ -1,16 +1,32 @@
 import React, {useState} from 'react';
 import {StyleSheet, Pressable, View} from 'react-native';
 import {StyledText, StyledTextInput} from './name-form.styles';
+import {useProfile} from '../../features/profile';
+import {logger} from '../../services/logger';
 
 export function NameForm() {
   const [username, setUsername] = useState();
+  const [showSuccess, setShowSuccess] = useState(false);
+  const {updateName} = useProfile();
 
   function onChangeText(text) {
+    if (showSuccess) {
+      setShowSuccess(false);
+    }
     setUsername(text);
   }
 
   function onSetUsername() {
-    console.log('You want to set the username huh?');
+    try {
+      if (username) {
+        updateName(username);
+      }
+      setShowSuccess(true);
+    } catch (error) {
+      logger('onSetUsername()', error);
+    } finally {
+      setUsername('');
+    }
   }
 
   return (
@@ -24,6 +40,9 @@ export function NameForm() {
       />
 
       <Button onPress={onSetUsername}>Update username</Button>
+      {showSuccess && (
+        <StyledText>That worked! Go to second view to see it! </StyledText>
+      )}
     </View>
   );
 }
