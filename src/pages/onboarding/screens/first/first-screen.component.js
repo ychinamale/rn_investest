1738afa38383fca5bf91fed73fbb5007b28c96a3
Yamikani Chinamale <ychinamale@gmail.com>
2023-10-31 16:35:57 +0200

    Refactor 🤪

--- src/pages/onboarding/screens/first/first-screen.component.js
+++ src/pages/onboarding/screens/first/first-screen.component.js
@@ -1,66 +1,11 @@
-import React, {useState} from 'react';
-import {
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  View,
-} from 'react-native';
-import {StyledText} from './first-screen.styles';
-import colors from '../../../../app/colors';
-
-function Button({onPress, children}) {
-  return (
-    <Pressable
-      onPress={onPress}
-      style={({pressed}) => [
-        {backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white'},
-        s.wrapperCustom,
-      ]}>
-      <StyledText>{children}</StyledText>
-    </Pressable>
-  );
-}
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {NameForm} from '../../../../widgets/name-form';
 
 export function FirstScreen() {
-  const [username, setUsername] = useState();
-
-  function onChangeText(text) {
-    setUsername(text);
-  }
-
-  function onSetUsername() {
-    console.log('You want to set the username huh?');
-  }
-
   return (
     <SafeAreaView>
-      <View>
-        <StyledText>Please enter your name</StyledText>
-        <TextInput
-          style={{
-            borderWidth: 1,
-            alignSelf: 'flex-start',
-            width: 100,
-            height: 40,
-            color: colors.BASE_COLORS.BLACK,
-          }}
-          value={username}
-          onChangeText={onChangeText}
-          keyboardType="default"
-        />
-
-        <Button onPress={onSetUsername}>Update username</Button>
-      </View>
+      <NameForm />
     </SafeAreaView>
   );
 }
-
-const s = StyleSheet.create({
-  wrapperCustom: {
-    borderRadius: 8,
-    padding: 6,
-    alignSelf: 'flex-start',
-    marginVertical: 16,
-  },
-});
--- src/pages/onboarding/screens/first/first-screen.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-import colors from '../../../../app/colors';
-
-export const StyledText = styled.Text`
-  color: ${colors.BASE_COLORS.BLACK};
-`;
