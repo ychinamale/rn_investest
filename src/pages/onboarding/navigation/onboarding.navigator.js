ea4812bd5766ab38b1f7c42f1f8d06237e31411c
Yamikani Chinamale <ychinamale@gmail.com>
2023-10-31 21:17:57 +0200

    Show name from store

--- src/pages/onboarding/navigation/onboarding.navigator.js
+++ src/pages/onboarding/navigation/onboarding.navigator.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {FirstScreen, SecondScreen, ThirdScreen} from '../screens';
+import {NamePill} from '../../../widgets';
 
 const Drawer = createDrawerNavigator();
-
-import {FirstScreen, SecondScreen, ThirdScreen} from '../screens';
 
 const SCREEN_NAMES = {
   FIRST: '1️⃣ First View',
@@ -11,11 +11,17 @@ const SCREEN_NAMES = {
   THIRD: '3️⃣ Third View',
 };
 
+const showNamePill = () => <NamePill />;
+
 function OnboardingNavigator() {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name={SCREEN_NAMES.FIRST} component={FirstScreen} />
-      <Drawer.Screen name={SCREEN_NAMES.SECOND} component={SecondScreen} />
+      <Drawer.Screen
+        name={SCREEN_NAMES.SECOND}
+        component={SecondScreen}
+        options={{headerRight: showNamePill}}
+      />
       <Drawer.Screen name={SCREEN_NAMES.THIRD} component={ThirdScreen} />
     </Drawer.Navigator>
   );
