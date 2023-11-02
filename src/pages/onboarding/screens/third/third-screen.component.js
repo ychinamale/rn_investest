1fb5dfe72182c5b4208de1d6d040fe3f76a6ad46
Yamikani Chinamale <ychinamale@gmail.com>
2023-11-02 13:06:20 +0200

    ✨ slide button

--- src/pages/onboarding/screens/third/third-screen.component.js
+++ src/pages/onboarding/screens/third/third-screen.component.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import {View} from 'react-native';
-import {StyledView} from './third-screen.styles';
+import SlideButton from 'rn-slide-button';
+import {FontAwesomeIcon as FaIcon} from '@fortawesome/react-native-fontawesome';
+
+import {
+  slideButtonStyles,
+  StyledView,
+  StyledWrapper,
+} from './third-screen.styles';
 import {Button} from '../../../../shared/components/button';
 
+const showThumbIcon = () => {
+  return <FaIcon icon="fa-gem" color="white" size={18} />;
+};
+
 export function ThirdScreen() {
+  const onSlide = () => {
+    console.log('Slide into your DMS!');
+  };
+
   return (
     <StyledView>
       <View>
         <Button variant={'link'}>Press me</Button>
         <Button variant={'secondary'}>Press me</Button>
         <Button variant={'primary'}>Press me</Button>
+        <StyledWrapper>
+          <SlideButton
+            title="Slide me to continue"
+            {...slideButtonStyles}
+            onReachedToEnd={onSlide}
+            icon={showThumbIcon()}
+          />
+        </StyledWrapper>
       </View>
     </StyledView>
   );
--- src/pages/onboarding/screens/third/third-screen.styles.js
+++ src/pages/onboarding/screens/third/third-screen.styles.js
@@ -1,8 +1,30 @@
 import styled from 'styled-components/native';
 import {colors} from '../../../../app/colors';
+import {VARIANTS} from '../../../../app/constants';
 
 export const StyledView = styled.View`
   background-color: ${colors.background.default};
   flex: 1;
   justify-content: flex-end;
+`;
+
+export const slideButtonStyles = {
+  padding: 0,
+  height: 48,
+  borderRadius: 8,
+  containerStyle: {
+    backgroundColor: colors.button.backgroundColor(VARIANTS.LINK),
+    borderColor: colors.button.borderColor(VARIANTS.TERTIARY),
+    borderWidth: 1,
+  },
+  thumbStyle: {
+    backgroundColor: colors.button.backgroundColor(),
+  },
+  titleStyle: {
+    fontSize: 12,
+  },
+};
+
+export const StyledWrapper = styled.View`
+  margin-horizontal: 8px;
 `;
