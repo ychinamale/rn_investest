6349bf035a9f66c3974d5823332d8a62ac8b31d7
Yamikani Chinamale <ychinamale@gmail.com>
2023-11-04 13:53:42 +0200

    ♻️ just in case

--- src/app/environment.js
+++ src/app/environment.js
@@ -1,5 +1,6 @@
 import CONFIG from 'react-native-config';
 
 const skipSimulatorCheck = CONFIG.SIMULATOR_CHECK === 'DISABLED';
+console.log('SIMULATOR CHECK is =>', CONFIG.SIMULATOR_CHECK);
 
 export {skipSimulatorCheck};
