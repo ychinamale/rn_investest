e93c6bbe9aa8bbe830c42a71c42b2ab6725053f4
Yamikani Chinamale <ychinamale@gmail.com>
2023-11-04 13:08:27 +0200

    🎨 aesthetics and whatnot

--- src/shared/components/text-pill/text-pill.component.js
+++ src/shared/components/text-pill/text-pill.component.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Text, View} from 'react-native';
+import {colors} from '../../../app/colors';
 
-export function TextPill({children}) {
+export function TextPill({children, style = {}, containerStyle = {}}) {
+  const viewStyle = useMemo(
+    () => ({
+      borderRadius: 16,
+      backgroundColor: colors.background.default,
+      marginHorizontal: 8,
+      padding: 4,
+      paddingHorizontal: 8,
+      ...containerStyle,
+    }),
+    [containerStyle],
+  );
+
+  const textStyle = useMemo(
+    () => ({
+      color: colors.text.alternate,
+      fontSize: 12,
+      ...style,
+    }),
+    [style],
+  );
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>{children}</Text>
     </View>
   );
 }
